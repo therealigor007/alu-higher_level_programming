d746cc892c29bff5d125d2fe7bb931332eb9affc
therealigor007 <i.ntwali@alustudent.com>
2025-07-02 10:52:15 +0200

    fix: the indentation

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 print("{}".format(", ".join("{}{}".format(i, j) for i in range(10)
-                             for j in range(i + 1, 10))))
+                            for j in range(i + 1, 10))))
