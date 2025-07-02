390c6eaba8bafd03c8e8e73b6408d4ace775fbe7
therealigor007 <i.ntwali@alustudent.com>
2025-07-02 12:36:34 +0200

    refactor: remove empty spaces

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -4,56 +4,56 @@
 
 class Node:
     """A class that defines a node of a singly linked list."""
-    
+
     def __init__(self, data, next_node=None):
         """Initialize a node.
-        
+
         Args:
             data (int): The data of the node.
             next_node (Node): The next node.
         """
         self.data = data
         self.next_node = next_node
-    
+
     @property
     def data(self):
         """Get the data of the node.
-        
+
         Returns:
             int: The data of the node.
         """
         return self.__data
-    
+
     @data.setter
     def data(self, value):
         """Set the data of the node.
-        
+
         Args:
             value (int): The data of the node.
-            
+
         Raises:
             TypeError: If value is not an integer.
         """
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
-    
+
     @property
     def next_node(self):
         """Get the next node.
-        
+
         Returns:
             Node: The next node.
         """
         return self.__next_node
-    
+
     @next_node.setter
     def next_node(self, value):
         """Set the next node.
-        
+
         Args:
             value (Node): The next node.
-            
+
         Raises:
             TypeError: If value is not a Node object or None.
         """
@@ -64,19 +64,19 @@ class Node:
 
 class SinglyLinkedList:
     """A class that defines a singly linked list."""
-    
+
     def __init__(self):
         """Initialize an empty singly linked list."""
         self.__head = None
-    
+
     def sorted_insert(self, value):
         """Insert a new Node into the correct sorted position.
-        
+
         Args:
             value (int): The value to insert.
         """
         new_node = Node(value)
-        
+
         if self.__head is None or self.__head.data > value:
             new_node.next_node = self.__head
             self.__head = new_node
@@ -87,10 +87,10 @@ class SinglyLinkedList:
                 current = current.next_node
             new_node.next_node = current.next_node
             current.next_node = new_node
-    
+
     def __str__(self):
         """Return a string representation of the list.
-        
+
         Returns:
             str: The string representation of the list.
         """
