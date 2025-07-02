9cc49be37f4703323df0978e188fbb940a14722a
therealigor007 <i.ntwali@alustudent.com>
2025-07-02 10:34:44 +0200

    refactor: fix the sscript number03

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if chr(i) != 'q' and chr(i) != 'e':
-        print(chr(i), end="")
+print("{}".format("".join([chr(i) for i in range(97, 123) if chr(i) not in "qe"])), end="")
