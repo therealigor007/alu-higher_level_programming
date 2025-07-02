7249fbfee9c08dc9c11f89fb827d69a364f074d7
therealigor007 <i.ntwali@alustudent.com>
2025-07-02 12:16:50 +0200

    refactor: remove white space

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,10 +4,10 @@
 
 class Square:
     """A class that defines a square by its size."""
-  
+
     def __init__(self, size):
         """Initialize a square with size.
-        
+
         Args:
             size: The size of the square.
         """
