a77b08c3d62b94b836a592fcf964d6aff1949753
therealigor007 <i.ntwali@alustudent.com>
2025-07-02 10:41:20 +0200

    refactor: remove empty line

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-print("{}".format("".join([chr(i) for i in range(97, 123)
-                           if chr(i) not in "qe"])), end="")
+print("{}".format("".join(chr(i) for i in range(97, 123) if i != 101 and i != 113)), end="")
