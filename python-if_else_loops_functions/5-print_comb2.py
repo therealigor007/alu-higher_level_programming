29be7c69b32de983b69a5dcfc0e67d03515c2552
therealigor007 <i.ntwali@alustudent.com>
2025-07-02 10:47:42 +0200

    fix: the file number05

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i == 99:
-        print(f"{i:02d}")
-    else:
-        print(f"{i:02d}", end=", ")
+print("{}".format(", ".join("{:02d}".format(i) for i in range(100))))
